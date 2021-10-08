64c04db9727df0bd1c6ad8ab6e3521e33accb99d
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-08 09:56:30 -0700

    completed employee table

--- extra_credit.sql
+++ extra_credit.sql
@@ -1,7 +1,8 @@
 -- Extra Credit
 -- SQL
--- Artist Table
 
+
+-- Artist Table
 -- Select 10 artists in reverse alphabetical order.
 SELECT *
 FROM artist
@@ -13,3 +14,9 @@ WHERE name LIKE 'Black%';
 -- Select all artists that contain the word ‘Black’.
 SELECT * FROM artist
 WHERE name LIKE '%Black%';
+
+-- Employee Table
+-- Find the birthdate for the youngest employee.
+SELECT MAX(birth_date) FROM employee;
+-- Find the birthdate for the oldest employee.
+SELECT MIN(birth_date) FROM employee;
