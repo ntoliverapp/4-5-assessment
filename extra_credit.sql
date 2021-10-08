3bf14323d9f6f2121904188786b3a64d84454c0c
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-08 10:10:18 -0700

    completed joins/subqueries extra credit

--- extra_credit.sql
+++ extra_credit.sql
@@ -27,3 +27,24 @@ SELECT COUNT(*) FROM invoice
 WHERE billing_state IN('CA', 'TX', 'AZ');
 -- Get the average total of the orders.
 SELECT AVG (total) FROM invoice;
+
+-- More Join Queries
+-- Get all playlist_track track_ids where the playlist name is Music.
+SELECT track_id, name FROM playlist_track
+JOIN playlist ON playlist.playlist_id = playlist_track.playlist_id;
+-- Get all track names for playlist_id 5.
+SELECT name FROM track
+WHERE track_id IN (
+SELECT track_id FROM playlist_track
+WHERE playlist_id = 5
+);
+
+-- Get all track names and the playlist name that they’re on ( 2 joins ).
+SELECT playlist.name, track.name FROM playlist
+JOIN playlist_track ON playlist_track.playlist_id = playlist.playlist_id
+JOIN track ON track.track_id = playlist_track.track_id;
+-- Get all track names and album titles that are the genre Alternative & Punk ( 2 joins ).
+SELECT track.name, album.title FROM track
+JOIN album ON album.album_id = track.album_id
+JOIN genre ON genre.genre_id = track.genre_id
+WHERE genre LIKE 'Alternative & Punk';
